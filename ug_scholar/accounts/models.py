664a4f2ebdd36bfadf3b87b8e8708a2cb0b634e6
DevPrinceK <80714449+DevPrinceK@users.noreply.github.com>
2023-10-07 09:01:11 +0000

    Update models.py

--- ug_scholar/accounts/models.py
+++ ug_scholar/accounts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from accounts.manager import AccountManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=100, unique=True)
+    fullname = models.CharField(max_length=100, blank=True, null=True)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    
+    objects = AccountManager()
+
+    USERNAME_FIELD = 'email'
+
+    def __str__(self):
+        return self.fullname or self.email 
+
+    class Meta:
+        db_table = 'user'
