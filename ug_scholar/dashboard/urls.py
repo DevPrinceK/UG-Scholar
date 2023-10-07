d8129f5726d6dbce52c9cdb8c526858cc0b5bc7c
DevPrinceK <80714449+DevPrinceK@users.noreply.github.com>
2023-10-07 09:03:08 +0000

    Update urls.py

--- ug_scholar/dashboard/urls.py
+++ ug_scholar/dashboard/urls.py
@@ -1,12 +1,22 @@
 from django.urls import path
 from . import views
 
+app_name = "dashboard"
+
+# dashboard
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+]
+
+# author and publications
+urlpatterns += [
     path('authors/', views.AuthorsView.as_view(), name='authors'),
     path('publications/', views.PublicationsView.as_view(), name='publications'),
+]
+
+# breakdown
+urlpatterns += [
     path('colleges/', views.CollegesView.as_view(), name='colleges'),
     path('faculties/', views.FacultiesView.as_view(), name='faculties'),
     path('departments/', views.DepartmentsView.as_view(), name='departments'),
-    path('login/', views.LoginView.as_view(), name='login'),
 ]
