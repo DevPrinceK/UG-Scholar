cc0efe67b69d9cdd903b5455530cfcb178ede122
DevPrinceK <80714449+DevPrinceK@users.noreply.github.com>
2023-10-15 17:42:52 +0000

    Update views.py

--- ug_scholar/accounts/views.py
+++ ug_scholar/accounts/views.py
@@ -1,9 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
     
 class LoginView(View):
     '''Login view - /login/'''
-    template_name = 'pages/login.html'
     
     def get(self, request):
-        return render(request, self.template_name)
+        # redirect to home when user uses the get method
+        return redirect("dashboard:index")
+    
+    
+    def post(self, request):
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request, email=email, password=password)
+        if user:
+            login(request, user)
+            messages.info(request, f"Successfully logged in as {user.fullname}")
+            return redirect("dashboard:index")
+        else:
+            messages.error(request, "Invalid email or password")
+            return redirect("dashboard:index")
+        
+        
+class LogoutView(View):
+    '''Logout view - Logout the current user'''
+    
+    def get(self, request):
+        logout(request)
+        messages.info(request, "Successfully logged out")
+        return redirect("dashboard:index")
+    
