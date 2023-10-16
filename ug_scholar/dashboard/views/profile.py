bf3d0cc521fde6f285df71aeb1d9f4f75ec3a257
DevPrinceK <80714449+DevPrinceK@users.noreply.github.com>
2023-10-16 09:10:47 +0000

    Update profile.py

--- ug_scholar/dashboard/views/profile.py
+++ ug_scholar/dashboard/views/profile.py
@@ -5,6 +5,7 @@ from django.http import HttpResponseRedirect
 from django.utils.decorators import method_decorator
 
 from ug_scholar.library.decorators import AdministratorsOnly
+from ug_scholar.library.utils_functions import log_user_action
 
 
 class ProfileView(View):
@@ -24,6 +25,7 @@ class ProfileView(View):
         password2 = request.POST.get('password2')
         
         if password != password2:
+            log_user_action(user, "Attempted to change password but passwords did not match")
             messages.error(request, "Passwords do not match")
             return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
         
@@ -43,7 +45,7 @@ class ProfileView(View):
         if password:
             user.set_password(password)
             user.save()
-        
+        log_user_action(user, "Updated their profile")
         messages.info(request, "Profile Updated Successfully")
         return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
     
@@ -61,8 +63,10 @@ class ChangeProfilePictureView(View):
         if profile_picture:
             user.profile_picture = profile_picture
             user.save()
+            log_user_action(user, "Updated their profile picture")
             messages.info(request, "Profile Picture Updated Successfully")
             return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
         else:
+            log_user_action(user, "Attempted to update their profile picture but no picture was uploaded")
             messages.error(request, "Profile Picture Not Updated")
             return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
