658e48715305a99351c5bca25196980d475a9520
princek <princesamuelpks@gmail.com>
2026-07-28 08:45:05 +0000

    Update forms.py

--- ug_scholar/dashboard/forms.py
+++ ug_scholar/dashboard/forms.py
@@ -6,4 +6,15 @@ from api.models import Profile
 class AuthorProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        exclude = ['thumbnail', 'statistics', ]
+        fields = [
+            "name",
+            "scholar_id",
+            "orcid",
+            "source_author_id",
+            "affiliation",
+            "rank",
+            "email",
+            "college",
+            "school",
+            "department",
+        ]
