4800b551dfc04b0e3b7adccaf9e884818f14d454
princek <princesamuelpks@gmail.com>
2026-07-28 08:44:25 +0000

    Update admin.py

--- ug_scholar/api/admin.py
+++ ug_scholar/api/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Author, Profile, Publication
+from .models import Author, Profile, Publication, SyncRun
 
 admin.site.register(Publication)
+admin.site.register(SyncRun)
 admin.site.register(Profile)
 admin.site.register(Author)
